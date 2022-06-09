c21fb4506f90223e6971ace8da09f5182412cb62
Haste <91929257+Haste427@users.noreply.github.com>
2022-06-09 21:07:51 +0700

    Add files via upload

---
+++ latian/src/main/kotlin/bilaganbulat.kt
@@ -0,0 +1,9 @@
+fun main() {
+    var angka = arrayOf(5, 10, 13, 19, 21)
+    for(i = 0; i<= angka.length){
+        if (angka[i] % 3 == 0 ){
+            println(" ")
+        }
+    }
+
+}
---
+++ latian/src/main/kotlin/simetris.kt
@@ -0,0 +1,6 @@
+fun main () {
+    val bulat = 1234321;
+    val bulat2 = 1223322;
+    println(bulat >= 0);
+    println(bulat2 <= 0);
+}
