4741875b487a22f5b153e2f410b132cf0d603dd9
Haste <91929257+Haste427@users.noreply.github.com>
2022-07-04 19:18:45 +0700

    Add files via upload

---
+++ UAS/Mahasiswa/src/main/kotlin/listmahasiswa.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class listmahasiswa {
+    private JPanel panelMenu;
+    private JTextField txtNim;
+    private JTextField txtNama;
+    private JTextField txtEmail;
+    private JTable table1;
+    private JButton btntambah;
+    private JButton btnupdate;
+    private JButton btnhapus;
+}
